049bd0a9c464c3053cd3474040d225eeb2b80a5c
Lucas <lucasrclark@gmail.com>
2022-09-28 19:54:30 -0500

    Added Activities Selector

---
+++ Assignment 1/activityselector.js
@@ -0,0 +1,79 @@
+function NZ() {
+  console.log("test NZ")
+  document.getElementById("activitydisplay").style.display = "flex";
+  document.getElementById("location").innerHTML = "New Zealand";
+
+  document.getElementById("checkboxes").innerHTML = 
+  "<input type=\"checkbox\"> City Tours </input>" 
+
+  document.getElementById("checkboxes").innerHTML += 
+  "<input type=\"checkbox\"> Sports </input>"
+  
+  document.getElementById("checkboxes").innerHTML += 
+  "<input type=\"checkbox\"> Cycling </input>" 
+  
+  document.getElementById("checkboxes").innerHTML += 
+  "<input type=\"checkbox\"> Museums </input>" 
+  
+  document.getElementById("checkboxes").innerHTML += 
+  "<input type=\"checkbox\"> Boating </input>";
+}
+
+function Maldives() {
+  console.log("test Maldives")
+  document.getElementById("activitydisplay").style.display = "flex";
+  document.getElementById("location").innerHTML = "Maldives, South Asia";
+
+  document.getElementById("checkboxes").innerHTML = 
+  "<input type=\"checkbox\"> Museums </input>" 
+
+  document.getElementById("checkboxes").innerHTML += 
+  "<input type=\"checkbox\"> Sailing </input>"
+  
+  document.getElementById("checkboxes").innerHTML += 
+  "<input type=\"checkbox\"> Beach </input>" 
+  
+  document.getElementById("checkboxes").innerHTML += 
+  "<input type=\"checkbox\"> Hiking </input>" 
+  
+  document.getElementById("checkboxes").innerHTML += 
+  "<input type=\"checkbox\"> Boating </input>";
+}
+
+function Venice() {
+  console.log("test Venice")
+  document.getElementById("activitydisplay").style.display = "flex";
+  document.getElementById("location").innerHTML = "Venice, Italy";
+
+  document.getElementById("checkboxes").innerHTML = 
+  "<input type=\"checkbox\"> Museums </input>" 
+
+  document.getElementById("checkboxes").innerHTML += 
+  "<input type=\"checkbox\"> Theatre </input>"
+  
+  document.getElementById("checkboxes").innerHTML += 
+  "<input type=\"checkbox\"> Parks and Recreation </input>" 
+  
+  document.getElementById("checkboxes").innerHTML += 
+  "<input type=\"checkbox\"> City Tours </input>" 
+  
+}
+
+function Cancun() {
+  console.log("test Cancun")
+  document.getElementById("activitydisplay").style.display = "flex";
+  document.getElementById("location").innerHTML = "Cancun";
+
+  document.getElementById("checkboxes").innerHTML = 
+  "<input type=\"checkbox\"> Parks and Recreation </input>" 
+
+  document.getElementById("checkboxes").innerHTML += 
+  "<input type=\"checkbox\"> Beaches </input>"
+  
+  document.getElementById("checkboxes").innerHTML += 
+  "<input type=\"checkbox\"> Boating </input>" 
+  
+  document.getElementById("checkboxes").innerHTML += 
+  "<input type=\"checkbox\"> Snorkeling </input>" 
+  
+}
