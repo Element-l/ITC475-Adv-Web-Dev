bba6125bbd3ccfb0bce3b1268a2800598a36ae5c
Lucas <lucasrclark@gmail.com>
2022-10-05 00:05:10 -0500

    Added more to contact form, validation, etc

--- Assignment 1/activityselector.js
+++ Assignment 1/activityselector.js
@@ -4,16 +4,16 @@ function NZ() {
   document.getElementById("location").innerHTML = "New Zealand";
 
   document.getElementById("checkboxes").innerHTML = 
-  "<input type=\"checkbox\"> City Tours </input>" 
+  "<input type=\"checkbox\"> City Tours </input><br>" 
 
   document.getElementById("checkboxes").innerHTML += 
-  "<input type=\"checkbox\"> Sports </input>"
+  "<input type=\"checkbox\"> Sports </input><br>"
   
   document.getElementById("checkboxes").innerHTML += 
-  "<input type=\"checkbox\"> Cycling </input>" 
+  "<input type=\"checkbox\"> Cycling </input><br>" 
   
   document.getElementById("checkboxes").innerHTML += 
-  "<input type=\"checkbox\"> Museums </input>" 
+  "<input type=\"checkbox\"> Museums </input><br>" 
   
   document.getElementById("checkboxes").innerHTML += 
   "<input type=\"checkbox\"> Boating </input>";
@@ -25,16 +25,16 @@ function Maldives() {
   document.getElementById("location").innerHTML = "Maldives, South Asia";
 
   document.getElementById("checkboxes").innerHTML = 
-  "<input type=\"checkbox\"> Museums </input>" 
+  "<input type=\"checkbox\"> Museums </input><br>" 
 
   document.getElementById("checkboxes").innerHTML += 
-  "<input type=\"checkbox\"> Sailing </input>"
+  "<input type=\"checkbox\"> Sailing </input><br>"
   
   document.getElementById("checkboxes").innerHTML += 
-  "<input type=\"checkbox\"> Beach </input>" 
+  "<input type=\"checkbox\"> Beach </input><br>" 
   
   document.getElementById("checkboxes").innerHTML += 
-  "<input type=\"checkbox\"> Hiking </input>" 
+  "<input type=\"checkbox\"> Hiking </input><br>" 
   
   document.getElementById("checkboxes").innerHTML += 
   "<input type=\"checkbox\"> Boating </input>";
@@ -46,13 +46,13 @@ function Venice() {
   document.getElementById("location").innerHTML = "Venice, Italy";
 
   document.getElementById("checkboxes").innerHTML = 
-  "<input type=\"checkbox\"> Museums </input>" 
+  "<input type=\"checkbox\"> Museums </input><br>" 
 
   document.getElementById("checkboxes").innerHTML += 
-  "<input type=\"checkbox\"> Theatre </input>"
+  "<input type=\"checkbox\"> Theatre </input><br>"
   
   document.getElementById("checkboxes").innerHTML += 
-  "<input type=\"checkbox\"> Parks and Recreation </input>" 
+  "<input type=\"checkbox\"> Parks and Recreation </input><br>" 
   
   document.getElementById("checkboxes").innerHTML += 
   "<input type=\"checkbox\"> City Tours </input>" 
@@ -65,15 +65,47 @@ function Cancun() {
   document.getElementById("location").innerHTML = "Cancun";
 
   document.getElementById("checkboxes").innerHTML = 
-  "<input type=\"checkbox\"> Parks and Recreation </input>" 
+  "<input type=\"checkbox\"> Parks and Recreation </input><br>" 
 
   document.getElementById("checkboxes").innerHTML += 
-  "<input type=\"checkbox\"> Beaches </input>"
+  "<input type=\"checkbox\"> Beaches </input><br>"
   
   document.getElementById("checkboxes").innerHTML += 
-  "<input type=\"checkbox\"> Boating </input>" 
+  "<input type=\"checkbox\"> Boating </input><br>" 
   
   document.getElementById("checkboxes").innerHTML += 
   "<input type=\"checkbox\"> Snorkeling </input>" 
   
+}
+
+function validateForm() {
+  let fname = document.forms["inputform"]["fname"].value;
+  let lname = document.forms["inputform"]["lname"].value;
+  let email = document.forms["inputform"]["email"].value;
+  let phone = document.forms["inputform"]["phone"].value;
+  let quantity = document.forms["inputform"]["quantity"].value;
+  let date = document.forms["inputform"]["date"].value;
+
+  if (fname === "" || lname === "" || email === "" || phone === "" || quantity === "" || date === ""){
+    document.getElementById("error").innerHTML = "<p>Empty Form Detected</p>";
+  }
+  else if (isChecked() == false){
+    document.getElementById("error").innerHTML = "<p>No activities selected</p>";
+  }
+  else if (/\D/.test(phone)){
+    document.getElementById("error").innerHTML = "<p>Phone number contains letters</p>";
+  }
+  else{
+    document.getElementById("error").innerHTML = "<p>Clear :)</p>";
+  }
+}
+
+function isChecked() {
+  let checkboxes = document.querySelectorAll('input[type=checkbox]');
+  let checkboxLength = checkboxes.length;
+  for (let i = 0; i < checkboxLength; i++) {
+    if (checkboxes[i].checked) return true;
+  }
+
+  return false;
 }
